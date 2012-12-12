6ad50b6677dc1417a48be838d192605defb91621
Viktor Koop <viktor.koop@googlemail.com>
2012-12-12 23:35:54 +0100

    handle "my.zsh" as a private config - should be ignored

--- load_excluded/oh-my-zsh-custom.symlink/my.zsh
+++
@@ -1,8 +0,0 @@
-# Add yourself some shortcuts to projects you often work on
-# Example:
-#
-# brainstormr=/Users/robbyrussell/Projects/development/planetargon/brainstormr
-#
-
-qpg=/Users/viktor/Entwicklung/repositories/uni/pg/MAGIC/Group2
-qwork=/Users/viktor/Entwicklung/repositories/work/webarena/source
