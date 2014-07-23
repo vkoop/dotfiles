5837417fed02fc579ad92c01209fbb438cd236e2
Viktor Koop <viktor.koop@gmail.com>
2014-07-23 22:52:54 +0200

    bash setup script

---
+++ setup/setup.sh
@@ -0,0 +1,57 @@
+#!/bin/sh
+
+echo 'Starting setup';
+
+REALPATH=$(grealpath -zq "$PWD");
+
+LINKABLES=$(ls -d $REALPATH/**/*.symlink); #-d --> shows only directories instead of contents
+SKIPALL=false;
+OVERWRITEALL=false;
+BACKUPALL=false;
+
+for LINK in $LINKABLES
+do
+	echo "Found file to link: $LINK"
+	FILENAME=$(basename $LINK .symlink) #TODO ordner muss noch entfernt werden 
+	TARGET="$HOME/.$FILENAME"
+	BACKUP=false
+	OVERWRITE=false
+
+	echo "target: $TARGET"
+
+	if [ -a $TARGET ];
+	then
+		echo "file already exists: $TARGET";
+		if [ $SKIPALL = false ] && [ $OVERWRITEALL = false ] && [ $BACKUPALL = false ]; 
+		then
+			#show dialog
+			select selection in "skip" "skip all" "overwrite" "overwrite all" "backup" "backup all" ; do
+				case $selection in
+					"skip" ) continue 2;;
+					"skip all" ) SKIPALL=true; break 2;;
+					"overwrite" ) OVERWRITE=true; break;;
+					"overwrite all" ) OVERWRITEALL=true; break;;
+					"backup" ) BACKUP=true; break;;
+					       "backup all" ) BACKUPALL=true; break;;	
+				esac
+			done
+		fi
+	fi
+
+	if $BACKUP || $BACKUPALL ;  then
+		echo "trying to create backup $TARGET.backup";
+		mv "$TARGET" "$TARGET.backup";
+	fi
+
+	if $OVERWRITE  || $OVERWRITEALL ; then
+		if [ -f $TARGET ]; then
+			echo "overwriting $TARGET";
+			rm $TARGET;
+		else
+			echo "overwriting $TARGET";
+			rm -R $TARGET;
+		fi
+	fi	
+	echo "Creating symlink from: $LINK to $TARGET";
+	ln -s $LINK $TARGET #perhaps have to get realpath of LINK
+done
